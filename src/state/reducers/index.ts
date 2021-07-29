79f24ec3c15b9c9b7378f37a9e69997c4edd31dc
sksurendrakmr <sksurendrakmr@gmail.com>
2021-07-29 19:24:00 +0530

    Redux with typescript

---
+++ src/state/reducers/index.ts
@@ -0,0 +1,13 @@
+import repositoriesReducer from './repositoriesReducer'
+import {combineReducers} from 'redux'
+
+const reducers = combineReducers({
+    repositories:repositoriesReducer
+})
+
+
+export default reducers
+
+
+export type RootState = ReturnType<typeof reducers>
+//give the return types of reducers function
